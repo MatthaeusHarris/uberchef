032be7536cfe681e0ed04301507e74b7f25eb5e8
Matt Harris <matthaeus.harris@gmail.com>
2011-11-02 11:56:57 -0600

    Update the rcg_user def'n to use userdatabag attr
    Conflicts:
    
    	uberuser/definitions/rcg_user.rb

--- uberuser/definitions/rcg_user.rb
+++ uberuser/definitions/rcg_user.rb
@@ -1,13 +1,8 @@
 require "pp"
 
 define :rcg_user do
-<<<<<<< HEAD
-  pp params
-  useritem = data_bag_item("users",params[:name])
-=======
 #  pp params
   useritem = data_bag_item(node[:uberuser][:userdatabag],params[:name])
->>>>>>> 856a0b0... Update the rcg_user def'n to use userdatabag attr
 #  pp useritem["ssh"]["dsa_id"]
   useritem["username"] = params[:name] unless useritem["username"]
   useritem["home"] = "/home/" + useritem["username"] unless useritem["home"]
