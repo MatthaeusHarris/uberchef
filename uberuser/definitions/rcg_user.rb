6c36c117aa24038a52b48131fe870a9a951f5232
Matt Harris <raven@uberduck.net>
2011-11-01 21:26:27 -0600

    Typo.  :(

--- uberuser/definitions/rcg_user.rb
+++ uberuser/definitions/rcg_user.rb
@@ -3,7 +3,7 @@ require "pp"
 define :rcg_user do
   pp params
   useritem = data_bag_item("users",params[:name])
-  pp useritems
+  pp useritem
 #  pp useritem["ssh"]["dsa_id"]
   useritem["username"] = params[:name] unless useritem["username"]
   user useritem["username"] do
