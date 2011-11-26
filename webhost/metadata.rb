cc2f735db31ed6c80e3aa24ac56d9fa9c2cad8f5
Matt Harris <raven@uberduck.net>
2011-11-26 15:23:48 -0700

    Removed other dependencies.

--- webhost/metadata.rb
+++ webhost/metadata.rb
@@ -5,5 +5,3 @@ description      "Uses providers in the opsocde apache2 recipe to set up virtual
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 depends          "apache2"
-depends          "apache2::php5"
-depends          "apache2::mod_rewrite"
