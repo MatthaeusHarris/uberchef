4582ed6f37fb5730bc98620b8f2266587f149cbe
Matt Harris <matthaeus.harris@gmail.com>
2011-12-02 16:45:38 -0700

    Rev 1 complete.

--- webproxy/recipes/default.rb
+++ webproxy/recipes/default.rb
@@ -2,7 +2,33 @@
 # Cookbook Name:: webproxy
 # Recipe:: default
 #
-# Copyright 2011, YOUR_COMPANY_NAME
+# Copyright 2011, Matt Harris
 #
-# All rights reserved - Do Not Redistribute
+# All rights reserved
 #
+
+require "pp"
+
+webhosts = search(:node, "virtualhosts:*")
+
+webhosts.each do |w|
+  pp w["ipaddress"]
+  w['virtualhosts'].each do |k, v|
+    pp v
+    template "/etc/apache2/sites-available/#{k}.conf" do
+      source "proxyhost.erb"
+      variables(
+        :servername => v['server_name'],
+        :serveraliases => v['server_aliases'],
+        :hostip => w['ipaddress']
+      )
+      notifies :reload, "service[apache2]"
+    end
+    
+    link "/etc/apache2/sites-enabled/#{k}.conf" do
+      to "/etc/apache2/sites-available/#{k}.conf"
+      notifies :reload, "service[apache2]"
+    end
+    
+  end
+end
