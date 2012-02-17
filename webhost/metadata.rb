2ad8391e07d3f75040e30f08a801a30c677584d6
Matt Harris <raven@uberduck.net>
2012-02-16 17:48:08 -0700

    Fixed syntax in virthost.conf

--- webhost/metadata.rb
+++ webhost/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "raven@uberduck.net"
 license          "All rights reserved"
 description      "Uses providers in the opsocde apache2 recipe to set up virtualhosts"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 depends          "apache2"
